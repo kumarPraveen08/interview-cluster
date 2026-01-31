c83cb7f994cc30db5a989564b156b5965c2eeb27
kumarPraveen08 <pk20210845@gmail.com>
2026-01-31 15:19:55 +0530

    some changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/interview-cluster',
+  assetPrefix: '/interview-cluster',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
